2b58fa18f078525f0a3fb0d2f71e25f6a9fbdcb2
bhartibhatia <bhartibhatia1997@gmail.com>
2020-12-10 23:23:27 +0530

    Working On Movie Trailer

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import "./Nav.css"
 function Nav() {
+    const [show,handelShow]=useState(false);
+
+    useEffect(() =>{
+        window.addEventListener("scroll", () =>{
+            if(window.scrollY >100)
+            {
+                handelShow(true);
+            }else handelShow(false);
+        });
+        return () =>{
+            window.removeEventListener("scroll");
+        }
+        },[]);
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav_black"}`}>
 <img className="nav_logo"
 src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
  alt="Netflix-Logo"
  />
  <img
  className="nav_avatar"
- src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+ src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
   alt="Netflix-Logo"
  />
             
--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,5 @@
 import React ,{useState,useEffect} from 'react'
+import YouTube from 'react-youtube';
 import axios from "./axios"
 import "./Row.css"
 
@@ -20,7 +21,18 @@ fetchData().catch( (error)=>{
 console.log(error)
 })
     },[props.fetchUrl]
-)
+);
+
+const opts = {
+    height:"390",
+    width:"100%",
+    playerVars:
+    {autoplay:1},
+
+}
+ const handelClick = (movie) =>{
+
+ }
 // console.log(movies);
     return (
         <div className="row">
@@ -30,11 +42,13 @@ console.log(error)
                    movies.map(
                       (movie) => (
 <img key={movie.id}
+onClick={() => handelClick(movie)}
  className={`row_posters_image ${props.isLargeRow && "row_posters"}`}
  src={`${BaseUrl}${props.isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
                       ) )
                }
             </div>
+            <YouTube videoId={trailerUrl} opts={opts}/>
         </div>
     )
 }
